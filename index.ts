0cc89c1fa383dd9ba116f2df9786eca9ba3627ee
ad <adolmit@gmail.com>
2020-06-19 17:32:47 -0300

    Primer commit rest server

---
+++ index.ts
@@ -0,0 +1,21 @@
+import Server from "./classes/server";
+import { Router } from "express";
+import router from "./routes/router";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+
+//body parser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+//cors
+server.app.use(cors({ origin: true, credentials: true }));
+
+//rutas de servicios
+server.app.use('/', router);
+
+server.start(() => {
+    console.log("Seervidor iniciado en " + server.port);
+});
